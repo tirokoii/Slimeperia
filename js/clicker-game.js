251d67e4ec18fa8c29bb5d97d1bf58904626e919
tirokoii <tilli.edstrom@elev.ga.ntig.se>
2025-03-25 15:08:20 +0100

    img, upgradebox, javscript

---
+++ js/clicker-game.js
@@ -0,0 +1,349 @@
+/* Med document.queryselector(selector) kan vi hämta
+ * de element som vi behöver från html dokumentet.
+ * Vi spearar elementen i const variabler då vi inte kommer att
+ * ändra dess värden.
+ * Läs mer:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+ * Viktigt: queryselector ger oss ett html element eller flera om det finns.
+ */
+const clickerButton = document.querySelector('#game-button');
+const moneyTracker = document.querySelector('#money');
+const ss = document.querySelector('#sold-slimes');
+const mpsTracker = document.querySelector('#mps'); // money per slime
+const spcTracker = document.querySelector('#spc'); // slime per click
+const spsTracker = document.querySelector('#sps'); // slime per second
+const upgradesTracker = document.querySelector('#upgrades');
+const upgradeList = document.querySelector('#upgradelist');
+const msgbox = document.querySelector('#msgbox');
+const audioAchievement = document.querySelector('#swoosh');
+
+/* Följande variabler använder vi för att hålla reda på hur mycket pengar som
+ * spelaren, har och tjänar.
+ * last används för att hålla koll på tiden.
+ * För dessa variabler kan vi inte använda const, eftersom vi tilldelar dem nya
+ * värden, utan då använder vi let.
+ * Läs mer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+ */
+let slime = 0;
+let money = 0;
+let moneyPerClick = 1;
+let moneyPerSecond = 0;
+let acquiredUpgrades = 0;
+let last = 0;
+let numberOfClicks = 0; // hur många gånger har spelare eg. klickat
+let active = false; // exempel för att visa att du kan lägga till klass för att indikera att spelare får valuta
+
+// likt upgrades skapas här en array med objekt som innehåller olika former
+// av achievements.
+// requiredSOMETHING är vad som krävs för att få dem
+
+let achievements = [
+    {
+        description: 'Stor byggarn',
+        requiredUpgrades: 1,
+        acquired: false,
+    },
+    {
+        description: 'Galen vetenskaps man',
+        requiredUpgrades: 100,
+        acquired: false,
+    },
+    {
+        description: 'Tillbaka till fabriken',
+        requiredUpgrades: 1000,
+        acquired: false,
+    },
+    {
+        description: 'En otydlig start',
+        requiredClicks: 1,
+        acquired: false,
+    },
+    {
+        description: 'Nu börjar det likna något!',
+        requiredClicks: 100,
+        acquired: false,
+    },
+    {
+        description: 'En vägg byggd av slime',
+        requiredClicks: 1000,
+        acquired: false,
+    },
+    {
+        description: 'Slime doctor',
+        requiredClicks: 10000,
+        acquired: false,
+    },
+    {
+        description: 'Nu börjar du bli girig',
+        requiredClicks: 10001,
+        acquired: false,
+    },
+];
+
+/* Med ett valt element, som knappen i detta fall så kan vi skapa listeners
+ * med addEventListener så kan vi lyssna på ett specifikt event på ett html-element
+ * som ett klick.
+ * Detta kommer att driva klickerknappen i spelet.
+ * Efter 'click' som är händelsen vi lyssnar på så anges en callback som kommer
+ * att köras vi varje klick. I det här fallet så använder vi en anonym funktion.
+ * Koden som körs innuti funktionen är att vi lägger till moneyPerClick till
+ * money.
+ * Läs mer: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+ */
+clickerButton.addEventListener(
+    'click',
+    () => {
+        // vid click öka score med moneyPerClick
+        money += slime * slimePerClick;
+        // håll koll på hur många gånger spelaren klickat
+        numberOfClicks += 1;
+        // console.log(clicker.score);
+    },
+    false
+);
+
+/* För att driva klicker spelet så kommer vi att använda oss av en metod som heter
+ * requestAnimationFrame.
+ * requestAnimationFrame försöker uppdatera efter den refresh rate som användarens
+ * maskin har, vanligtvis 60 gånger i sekunden.
+ * Läs mer: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+ * funktionen step används som en callback i requestanaimationframe och det är
+ * denna metod som uppdaterar webbsidans text och pengarna.
+ * Sist i funktionen så kallar den på sig själv igen för att fortsätta uppdatera.
+ */
+function step(timestamp) {
+    moneyTracker.textContent = Math.round(money);
+    spsTracker.textContent = slimePerSecond;
+    spcTracker.textContent = slimePerClick;
+    mpsTracker.textContent = moneyPerSlime
+    upgradesTracker.textContent = acquiredUpgrades;
+
+    if (timestamp >= last + 1000) {
+        money += slimePerSecond;
+        last = timestamp;
+    }
+
+    if (slimePerSecond > 0 && !active) {
+        spsTracker.classList.add('active');
+        active = true;
+    }
+
+    // achievements, utgår från arrayen achievements med objekt
+    // koden nedan muterar (ändrar) arrayen och tar bort achievements
+    // som spelaren klarat
+    // villkoren i första ifsatsen ser till att achivments som är klarade
+    // tas bort. Efter det så kontrolleras om spelaren har uppfyllt kriterierna
+    // för att få den achievement som berörs.
+    achievements = achievements.filter((achievement) => {
+        if (achievement.acquired) {
+            return false;
+        }
+        if (
+            achievement.requiredUpgrades &&
+            acquiredUpgrades >= achievement.requiredUpgrades
+        ) {
+            achievement.acquired = true;
+            message(achievement.description, 'achievement');
+            return false;
+        } else if (
+            achievement.requiredClicks &&
+            numberOfClicks >= achievement.requiredClicks
+        ) {
+            achievement.acquired = true;
+            message(achievement.description, 'achievement');
+            return false;
+        }
+        return true;
+    });
+
+    window.requestAnimationFrame(step);
+}
+
+/* Här använder vi en listener igen. Den här gången så lyssnar iv efter window
+ * objeket och när det har laddat färdigt webbsidan(omvandlat html till dom)
+ * När detta har skett så skapar vi listan med upgrades, för detta använder vi
+ * en forEach loop. För varje element i arrayen upgrades så körs metoden upgradeList
+ * för att skapa korten. upgradeList returnerar ett kort som vi fäster på webbsidan
+ * med appendChild.
+ * Läs mer:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+ * https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+ * Efter det så kallas requestAnimationFrame och spelet är igång.
+ */
+window.addEventListener('load', (event) => {
+    upgrades.forEach((upgrade) => {
+        upgradeList.appendChild(createCard(upgrade));
+    });
+    window.requestAnimationFrame(step);
+});
+
+/* En array med upgrades. Varje upgrade är ett objekt med egenskaperna name, cost
+ * och amount. Önskar du ytterligare text eller en bild så går det utmärkt att
+ * lägga till detta.
+ * Läs mer:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+ */
+
+upgrades = [
+    {
+        name: 'Grön slime',
+        image: 'green-slime.png',
+        cost: 10,
+        amount: 1,
+    },
+    {
+        name: 'red-slime',
+        image: '../img/red-slime.png',
+        cost: 50,
+        clicks: 2,
+    },
+    
+    {
+        name: 'Blå slime',
+        image: '../img/blue-slime.png',
+        cost: 100,
+        clicks: 10,
+    },
+    {
+        name: 'Lila slime',
+        image: '../img/purple-slime.png',
+        cost: 500,
+        clicks: 10,
+    },
+    {
+        name: 'one-smiling',
+        image: '../img/1-smiling.png',
+        cost: 1000,
+        amount: 100,
+    },
+    {
+        name: 'hundred-smilings',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'five-hundred-smilings',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'thousand-smilings',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'speed-upgrade-one',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'speed-upgrade-two',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'speed-upgrade-three',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+    {
+        name: 'speed-upgrade-four',
+        image: '../img/10-smiling.png',
+        cost: 1500,
+        amount: 100,
+    },
+];
+
+machines = [
+    {
+        name: mixer,
+        image: '',
+        cost: 1,
+        amount: 10
+    }
+]
+
+/* createCard är en funktion som tar ett upgrade objekt som parameter och skapar
+ * ett html kort för det.
+ * För att skapa nya html element så används document.createElement(), elementen
+ * sparas i en variabel så att vi kan manipulera dem ytterligare.
+ * Vi kan lägga till klasser med classList.add() och text till elementet med
+ * textcontent = 'värde'.
+ * Sedan skapas en listener för kortet och i den hittar vi logiken för att köpa
+ * en uppgradering.
+ * Funktionen innehåller en del strängar och konkatenering av dessa, det kan göras
+ * med +, variabel + 'text'
+ * Sist så fäster vi kortets innehåll i kortet och returnerar elementet.
+ * Läs mer:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+ * https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+ * https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ */
+function createCard(upgrade) {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    item.style.backgroundImage = "url(upgrade.image)";
+    const header = document.createElement('p');
+    header.classList.add('title');
+    const cost = document.createElement('p');
+    const upgradeImage = document.createElement("img")
+    const img = new Image()
+    img.src = upgrade.image
+    upgradeImage.src = `./img/${upgrade.image}`
+    
+    item.appendChild(upgradeImage)
+    if (upgrade.amount) {
+        header.textContent = `1`;
+    } else {
+        header.textContent = `2`;
+    }
+    cost.textContent = `3`;
+
+    item.addEventListener('click', (e) => {
+        if (money >= upgrade.cost) {
+            acquiredUpgrades++;
+            money -= upgrade.cost;
+            upgrade.cost *= 1.5;
+            cost.textContent = 'Köp för ' + upgrade.cost + ' smilings';
+            slimePerSecond += upgrade.amount ? upgrade.amount : 0;
+            slimePerClick += upgrade.clicks ? upgrade.clicks : 0;
+            message('Grattis du har köpt en uppgradering!', 'success');
+        } else {
+            message('Du har inte råd.', 'warning');
+        }
+    });
+
+    item.appendChild(header);
+    item.appendChild(cost);
+    return item;
+}
+
+/* Message visar hur vi kan skapa ett html element och ta bort det.
+ * appendChild används för att lägga till och removeChild för att ta bort.
+ * Detta görs med en timer.
+ * Läs mer:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+ * https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout
+ */
+function message(text, type) {
+    const p = document.createElement('p');
+    p.classList.add(type);
+    p.textContent = text;
+    msgbox.appendChild(p);
+    if (type === 'achievement') {
+        audioAchievement.play();
+    }
+    setTimeout(() => {
+        p.parentNode.removeChild(p);
+    }, 2000);
+}
